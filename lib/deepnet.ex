d801a6bb96aaf3b0a34e44f5ead59af0c6c61421
ToBeAmped <martinja@oregonstate.edu>
2018-08-25 14:58:55 -0700

    Add start/2 to Deepnet module

--- lib/deepnet.ex
+++ lib/deepnet.ex
@@ -1,18 +1,30 @@
 defmodule Deepnet do
+  use Application
+  
   @moduledoc """
-  Documentation for Deepnet.
+  `Deepnet` is a 3 layered neural network architecture fully implemented in Elixir.
+  It utilizes an Elixir agent to store the state of the process that is working
+  on a particular problem. This project is associated with the post on
+  Automating The Future regarding multi layered neural networks in Elixir.
   """
+
+  defstruct [
+    user_input[[1, 0, 0], [1, 0, 1]],
+    desired_target: [1, 1, 1]
+  ]
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Deepnet.hello
-      :world
-
+  Starts DeepNet
   """
-  def hello do
-    :world
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Deepnet.Network, [3, 3, 3], [function: :create]),
+    ]
+
+    opts = [strategy: :one_for_one, name: Deepnet.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
